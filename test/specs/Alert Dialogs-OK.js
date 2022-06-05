28b58e2bc4ad4843d401656e57ec3133598b9b49
marcos <marcoslira16@gmail.com>
2022-06-05 12:53:37 -0300

    Criação Primeiro fluxo

---
+++ test/specs/Alert Dialogs-OK.js
@@ -0,0 +1,30 @@
+describe("Primeiro fluxo", () => {
+    it("Verificar se o título da pasta é App", async () => {
+        const textoTitulo = await $("android.widget.TextView").getText();
+        expect(textoTitulo).toBe("App");
+    });
+
+    it("Verificar se o botão App funciona", async () => {
+       await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+       const visibilidadedeBotao =  await $("id=action_bar").isDisplayed(); //true;
+       expect(visibilidadedeBotao).toBe(true);
+    });
+
+    it("Verificar se o botão Alert Dialogs funciona", async () => {
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $("id=action_bar").click();
+        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
+        const visibilidadedeBotao =  await $('android=new UiSelector().text("App/Alert Dialogs").className("android.widget.TextView")').isDisplayed(); //true;
+        expect(visibilidadedeBotao).toBe(true);
+     });
+
+     it("Verificar se o botão Alert Dialogs funciona", async () => {
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        await $("id=action_bar").click();
+        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
+        await $("id=two_buttons").click();
+        const visibilidadedeBotao =  await $("id=alertTitle").isDisplayed(); //true;
+        expect(visibilidadedeBotao).toBe(true);
+     });
+
+});
--- test/specs/primeiroTeste.js
+++
@@ -1,5 +0,0 @@
-describe("Primeiro teste", () => {
-    it("Meu primeiro teste", () => {
-
-    });
-});
